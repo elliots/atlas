a65bfca404a5ed039336cc6324a25779ec7d9883
Elliot Shepherd <elliot@upowr.com.au>
2026-04-05 14:37:06 +1000

    fix up licenses on files created/edited since fork

--- sql/migrate/migrate.go
+++ sql/migrate/migrate.go
@@ -1,6 +1,8 @@
 // Copyright 2021-present The Atlas Authors. All rights reserved.
 // This source code is licensed under the Apache 2.0 license found
 // in the LICENSE file in the root directory of this source tree.
+//
+// Modifications Copyright 2026 Elliot Shepherd
 
 package migrate
 
--- sql/mysql/oss_test.go
+++ sql/mysql/oss_test.go
@@ -1,4 +1,4 @@
-// Copyright 2021-present The Atlas Authors. All rights reserved.
+// Copyright 2026 Elliot Shepherd. All rights reserved.
 // This source code is licensed under the Apache 2.0 license found
 // in the LICENSE file in the root directory of this source tree.
 
